05b39b0d1ea2d48e9fc801550dfb0559558c3e55
jbovone <58782618+jbovone@users.noreply.github.com>
2019-12-14 15:55:18 -0300

    correciones y tarea tres añadida

--- tareas/clase-3/tarea-clase-3.js
+++ tareas/clase-3/tarea-clase-3.js
@@ -7,10 +7,53 @@
 // Imprimir "Hola " y el nombre, " te llamás igual que mi ..."
 // Si no, simplemente imprimir "Hola " + nombre!
 
+
+let nombreUsuario
+const miNombre = 'Julian';
+const nombreAmigo = 'Jose';
+
+obtenerNombre();
+
+function obtenerNombre() {
+    nombreUsuario = prompt('¿Como te llamas?')
+
+    if ( nombreUsuario == false || nombreUsuario === null) {
+        while (nombreUsuario == false || nombreUsuario === null) { nombreUsuario = prompt('¿Como te llamas?') }
+    }
+    
+}
+
+
+saludar()
+
+function saludar() {
+    if (nombreUsuario.toLowerCase() === miNombre.toLowerCase()) {
+        alert(`Hola ${nombreUsuario} yo tambien me llamo ${miNombre}!!!`)
+    }
+
+    else if (nombreUsuario.toLowerCase() === nombreAmigo.toLowerCase()) {
+        alert(`Hola ${nombreUsuario} te llamas igual que mi amigo`)
+    }
+
+    else {
+        alert(`Hola ${nombreUsuario}!!!`)
+    }
+}
+
+
+//la interpolacion de variables en el string solo funciona con "`"
+//for (i=1 ; -1<=i>=1; i*-1) }
+//function obtenerNombre(){if(){} else{obtenerNombre()} } no me dio como resultado la funcion recursiva que queria.
+
 //Tarea 2:
 // Preguntar la edad del usuario
 // Hacerle saber si tiene más, menos ó la misma edad que nosotros.
 
+
+
+const edadUsuario = Number(prompt('¿Qué edad tienes?'))  
+if (edadUsuario === (34 || 35 || 36) ){alert('Tienes mas o menos mi misma edad')} //32<=edadUsuario<=38 probe un domino matematico asi, no funciono. 
+    else alert('Tenemos una diferencia de edad') 
 //Tarea 3:
 // Preguntarle al usuario si tiene documento, y que conteste con "si" o "no".
 // Si dice si, preguntarle la edad.
@@ -19,3 +62,17 @@
 // Si no tiene documento, no dejarlo entrar al bar.
 // Si no entendemos la respuesta, le decimos que no entendimos la respuesta.
 // Punto bonus: SI, NO, Si, No, si, no.
+
+let tieneDocumento 
+let suEdad
+
+
+
+ tieneDocumento=prompt('¿Tiene Documento?');
+
+if (tieneDocumento.toLowerCase() ==='si') {
+   suEdad=Number(prompt('¿Cual es su edad?'))
+}  
+if (suEdad>=18) {prompt('Bienvenido!!')} 
+
+    else {prompt('Get Lost!!')} //este es copipasteado porque ya lo habia hecho, es un poco mas primitivo que el anterior.
